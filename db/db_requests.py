5717a4cd58d73b95ce5a94adadc1516cdbaf0fe5
ivah-golubev <golubevivan0500@gmail.com>
2026-05-22 01:16:38 +0300

    Add popular courses per room query

--- db/db_requests.py
+++ db/db_requests.py
@@ -5,8 +5,26 @@ from enums.Day import Day
 def select_requests_by_day(cursor: sqlite3.Cursor, day: Day):
     query = """
     SELECT * FROM requests_rooms
-    WHERE lower(day) = ?
+    WHERE day = ?
     """
     cursor.execute(query, (day.value,))
     
+    return cursor.lastrowid
+
+def select_popular_courses_per_room(cursor: sqlite3.Cursor, multiplier):
+    query = """
+    SELECT room_name, course, count(course) as course_count
+    FROM requests_rooms GROUP BY room_id, course HAVING course_count > ( SELECT (
+        SELECT count(course) as room_course_count
+        FROM requests_rooms AS rr
+        WHERE rr.room_id = room_id
+        GROUP BY rr.room_id, rr.course
+        ORDER BY rr.room_id, room_course_count DESC
+        LIMIT 1
+    ) * ? )
+    ORDER BY room_id, course_count DESC
+
+    """
+    cursor.execute(query, (1 - multiplier,))
+
     return cursor.lastrowid
