de83e895bdadc7d0292b801edbd9e6b166918877
ivah-golubev <golubevivan0500@gmail.com>
2026-05-23 20:08:34 +0300

    Null values validation

--- analysis.py
+++ analysis.py
@@ -11,7 +11,26 @@ def validate_time_bounds(df: pd.DataFrame) -> pd.DataFrame:
     mask2 = df['start_minute'].between(0, 1440)
     mask3 = df['end_minute'].between(0, 1440)
 
-    return df.loc[mask1 & mask2 & mask3]
+    df = df.loc[mask1 & mask2 & mask3]
+
+    return df
+
+def validate_null_values(df: pd.DataFrame) -> pd.DataFrame:
+    # na_rows = df.loc[df.isna().any(axis=1)]
+    
+    if 'start_minute' in df.columns and 'end_minute' in df.columns:
+        df = df.dropna(subset=['start_minute', 'end_minute'])
+
+    if 'course' in df.columns:
+        df['course'] = df['course'].fillna('undefined')
+    if 'room_name' in df.columns:
+        df['room_name'] = df['room_name'].fillna('undefined')
+    if 'room_capacity' in df.columns:
+        df['room_capacity'] = df['room_capacity'].fillna(0)
+    if 'room_building' in df.columns:
+        df['room_building'] = df['room_building'].fillna('undefined')
+
+    return df
 
 def get_requests_within(day: Day, start_minute = 0, end_minute = 1440) -> pd.DataFrame:
     df = pd.read_sql_query(db_q.requests_within_day_interval_query, 
@@ -19,6 +38,7 @@ def get_requests_within(day: Day, start_minute = 0, end_minute = 1440) -> pd.Dat
                            params=(start_minute, end_minute, day))
     
     df.drop_duplicates(inplace=True)
+    df = validate_null_values(df)
     df = validate_time_bounds(df)
     
     return df
@@ -40,3 +60,4 @@ def get_rooms_time_stats(day: Day, start_minute = 0, end_minute = 1440) -> pd.Da
     ).round(2)
 
     return stats
+
